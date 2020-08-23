45e112e9f557ba5387c9abce4b84039c9fc1e688
gyoun-im <gyoun.im@student.uts.edu.au>
2020-08-24 01:32:08 +1000

    80 percent complete

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    string objName;
+    int i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objName = gameObject.name;
+        i = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(objName + ": " + i);
+        i++;
+    }
+}
