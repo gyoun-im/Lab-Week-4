1818ea95c755a3726d4cd726faa173f6fc8abf40
gyoun-im <gyoun.im@student.uts.edu.au>
2020-08-24 02:01:28 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -7,18 +7,35 @@ public class PrintAndHide : MonoBehaviour
     string objName;
     int i;
     public Renderer rend;
+    string objTag;
+    int random;
 
     // Start is called before the first frame update
     void Start()
     {
+        System.Random rnd = new System.Random();
         objName = gameObject.name;
         i = 0;
+        objTag = gameObject.tag;
+        random = rnd.Next(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (i == 100 && objTag.Equals("Red"))
+        {
+            gameObject.SetActive(false);
+        }
+        if (i == random && objTag.Equals("Blue"))
+        {
+            rend.enabled = false;
+        }
+
+
         Debug.Log(objName + ": " + i);
         i++;
+
+        
     }
 }
